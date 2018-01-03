6c7c8a141ab9323b80befd6449fb54980f0e8ad2
Cornel Ichim <ichimcornel@ymail.com>
2018-01-03 21:02:14 +0200

    Add project files.

---
+++ EqualityOp.EqualityMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EqualityOp.EqualityMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
