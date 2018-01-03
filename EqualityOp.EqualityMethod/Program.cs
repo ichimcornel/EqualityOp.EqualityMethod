a8142d05394160d0dc348abdc6474df9a53f923f
Cornel Ichim <ichimcornel@ymail.com>
2018-01-03 21:09:05 +0200

    Added methods

--- EqualityOp.EqualityMethod/Program.cs
+++ EqualityOp.EqualityMethod/Program.cs
@@ -4,9 +4,21 @@ namespace EqualityOp.EqualityMethod
 {
     class Program
     {
+        static bool AreIntsEqualOp(int x, int y)
+        {
+            return x == y;
+        }
+
+        static bool AreIntsEqualMethod(int x, int y)
+        {
+            return x.Equals(y);
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Method: "+ AreIntsEqualMethod(3,3));
+            Console.WriteLine("Operator: " + AreIntsEqualOp(3,3));
             Console.WriteLine("Hello World!");
         }
+
     }
 }
